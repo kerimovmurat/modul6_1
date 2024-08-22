f5873241dc51337dc2b6fc41fc7230ba0100ae0e
kerimovmurat <kerimovmurat@yandex.ru>
2024-08-22 13:16:43 +0300

    Add files via upload

---
+++ modul_6_1.py
@@ -0,0 +1,50 @@
+class Animal:
+    alive = True # проверка живой или нет
+    fed = False # голодный или сытый
+    def __init__(self, name):
+        self.name = name
+
+class Mammal(Animal):
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+class Predator(Animal):
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+
+class Plant:
+    edible = False # съедобное или нет
+    def __init__(self, name):
+        self.name = name
+
+
+class Flower(Plant):
+    pass
+class Fruit(Plant):
+    edible = True
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
